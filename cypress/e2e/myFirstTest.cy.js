e5117f4cb464b334ca7bd23417dab0a272cbbcf9
Nkereuwem03 <nkereuwem.udoudo1@gmail.com>
2025-05-31 00:26:44 +0100

    Add initial test for OrangeHRM login page title

---
+++ cypress/e2e/myFirstTest.cy.js
@@ -0,0 +1,11 @@
+describe("OrangeHRM Login Page Title", () => {
+  it("should display the correct page title", () => {
+    cy.visit("https://opensource-demo.orangehrmlive.com/");
+    cy.title().should("eq", "OrangeHRM");
+  });
+
+  it("should not display an incorrect page title", function () {
+    cy.visit("https://opensource-demo.orangehrmlive.com/");
+    cy.title().should("equal", "OrangeHRm Live");
+  });
+});
