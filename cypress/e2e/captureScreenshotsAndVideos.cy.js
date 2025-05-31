45e38f32d484c8d9d26c2b63e63dbb6f31af1aec
Nkereuwem03 <nkereuwem.udoudo1@gmail.com>
2025-05-31 01:25:51 +0100

    Add tests for manual and automatic screenshot/video capture

---
+++ cypress/e2e/captureScreenshotsAndVideos.cy.js
@@ -0,0 +1,18 @@
+describe("Screenshot and Video Capture in Cypress", function () {
+  it("captures screenshots manually", function () {
+    cy.visit("https://practice-automation.com/");
+    cy.screenshot("homepage");
+    cy.get(".wp-image-12372.sp-no-webp").screenshot("form field icon");
+  });
+
+  // automatically capture screenshot & video on failure - only when you execute through CLI
+
+  it("captures screenshots and videos automatically on test failure", function () {
+    cy.visit("https://practice-automation.com/");
+    cy.contains(
+      "a.wp-block-button__link.wp-element-button",
+      "Form Fields"
+    ).click();
+    cy.get("h1").should("have.text", "Form Field");
+  });
+});
