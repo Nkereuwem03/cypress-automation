4925a15caa687516a7d0e408ae9ac049aa5f0e64
Nkereuwem03 <nkereuwem.udoudo1@gmail.com>
2025-05-31 23:30:09 +0100

    Add ReadMe.md

---
+++ cypress/support/commands.js
@@ -0,0 +1,57 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+/// <reference types="Cypress" />
+
+// custom iframe command
+
+Cypress.Commands.add("getIframe", (iframe) => {
+  return cy
+    .get(iframe)
+    .its("0.contentDocument.body")
+    .should("be.visible")
+    .then(cy.wrap);
+});
+
+// custom commands for clicking on links using label
+
+Cypress.Commands.add("clickLink", (linkText) => {
+  return cy
+    .get(".inventory_item_name")
+    .contains(linkText, { matchCase: false })
+    .click()
+    .get(".inventory_details_name.large_size")
+    .should("have.text", "Sauce Labs Bike Light");
+});
+
+// custom login for 'customCommands.cy.js
+
+Cypress.Commands.add("loginApp", (url, username, password) => {
+  cy.visit(url);
+  cy.get("#user-name").type(username);
+  cy.get("#password").type(password);
+  cy.get("#login-button").click();
+});
