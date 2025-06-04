451789bab53c95be6f8b21b3c1d711094424f997
Nkereuwem03 <nkereuwem.udoudo1@gmail.com>
2025-06-04 19:34:56 +0100

    Update Cypress config: ensure mochawesome reporter options and video settings are clear

--- cypress.config.js
+++ cypress.config.js
@@ -1,24 +1,31 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
 
-  // You are free to customize any of the reporterOptions below to suit your reporting needs.
-  // For more information on the available options, please refer to the documentation: https://github.com/LironEr/cypress-mochawesome-reporter#options](https://github.com/LironEr/cypress-mochawesome-reporter#options)
+  reporter: 'cypress-mochawesome-reporter',
+  video: true,
+  videoUploadOnPasses: false,
+  retries: {
+    runMode: 3,
+    openMode: 3,
+  },
+  trashAssetsBeforeRuns: true,
+  globalTimeout: 60000,
+  // defaultCommandTimeout: 10000,
+  // pageLoadTimeout: 6000,
+  // requestTimeout: 15000,
+  // responseTimeout: 15000
+
   reporterOptions: {
     reportDir: 'cypress/reports/mochawesome',
-    overwrite: false,
+    overwrite: true,
     html: true,
     json: true,
     charts: true,
-    embeddedScreenshots: false,
+    embeddedScreenshots: true,
     inlineAssets: true,
     timestamp: 'dd/mm/yyyy_HH.MM.ss',
     saveAllAttempts: false,
-    video: true,
-    videoCompression: 32,
-    videoUploadOnPasses: false,
-    videosFolder: 'cypress/videos',
     // code: true,
     // reportPageTitle: 'Cypress Test Report',
     // reportFilename: 'cypress-test-report',
@@ -26,19 +33,6 @@ module.exports = defineConfig({
     // autoOpen: false,
     // mochaFile: 'cypress/reports/mocha-results.xml',
     // screenshotsFolder: 'cypress/screenshots',
-    // enableCharts: true,
-    // enableCode: true,
-    // enableInlineAssets: true,
-    // enableReportPageTitle: true,
-    // enableReportFilename: true,
-    // enableQuiet: true,
-    // enableAutoOpen: true,
-    // enableMochaFile: true,
-    // enableVideo: true,
-    // enableVideoCompression: true,
-    // enableVideoUploadOnPasses: true,
-    // enableScreenshotsFolder: true,
-    // enableVideosFolder: true,
   },
 
   e2e: {
@@ -47,10 +41,5 @@ module.exports = defineConfig({
       config.screenshotOnRunFailure = true;
       require('cypress-mochawesome-reporter/plugin')(on)
     }
-
-    // defaultCommandTimeout: 10000,
-    // pageLoadTimeout: 6000,
-    // requestTimeout: 15000,
-    // responseTimeout: 15000
   },
 });
